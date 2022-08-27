9092d09a2f5f3477c27b4260948c0910f30b384b
Anna Maria Lester <anna47c4@stud.kea.dk>
2022-08-27 22:44:13 +0200

    testing & exercising w. console

---
+++ console_datatypes_test.js
@@ -0,0 +1,21 @@
+/* "use strict";
+const bool = true;
+const num = 41;
+const str = "Anna";
+const obj = {
+  cats: 2,
+  cars: 1,
+};
+const nothing = null;
+let undf;
+const sym = symbol("symbol"); */
+
+/* number & string, when u make an addition in the console, it becomes clear that when u cross the two types of values one of them will convert  */
+/* example: n1+n2=3 (numbers, math) but s1+s2=12 (put the strings (text) together) */
+const n1 = 1;
+const n2 = 2;
+const n3 = 3;
+
+const s1 = "1";
+const s2 = "2";
+const s3 = "3";
---
+++ test_falsy_truthy.js
@@ -0,0 +1,6 @@
+const value = true;
+if ("") {
+  console.log("Value is truthy");
+} else {
+  console.log("Value is falsy");
+}
